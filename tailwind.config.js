9ba82d9e8a57b82d304852d2d2affefe5ccabda2
yoga-python <73431769+yoga-python@users.noreply.github.com>
2023-04-08 22:43:08 +0200

    add: extend tailwind theme with bg-images

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,29 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx}',
-    './src/components/**/*.{js,ts,jsx,tsx}',
-    './src/app/**/*.{js,ts,jsx,tsx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx}",
+    "./src/components/**/*.{js,ts,jsx,tsx}",
+    "./src/app/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+        "shop-bg-texture": "url('/clean-gray-paper.png')",
+      },
+      colors: {
+        hero: {
+          text: "hsl(100, 100%, 98%)",
+          primary: "hsla(8, 100%, 24%, 1)",
+        },
+        shop: {
+          text: "hsl(0, 0%, 0%)",
+          bg: "hsla(190, 0%, 90%, 1)",
+        },
       },
     },
   },
   plugins: [],
-}
+};
