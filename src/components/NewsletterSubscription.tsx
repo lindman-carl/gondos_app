9410c051200c7364653250f1f66fbc9d14d0b646
yoga-python <73431769+yoga-python@users.noreply.github.com>
2023-04-12 00:12:07 +0200

    add: max width

--- src/components/NewsletterSubscription.tsx
+++ src/components/NewsletterSubscription.tsx
@@ -13,30 +13,32 @@ const NewsletterSubscription = () => {
   };
 
   return (
-    <div className="flex w-full flex-col items-center justify-start gap-y-4 bg-shop-bg p-16 text-shop-text">
-      <h3 className="text-2xl font-bold">Subscribe</h3>
-      <span className="text-center text-sm font-light">
-        Sign up with your email address to receive news, updates and deals.
-      </span>
-      <form
-        onSubmit={handleSubmit}
-        className="flex w-full flex-col items-center justify-center gap-y-2"
-      >
-        <input
-          type="email"
-          placeholder="Email Address"
-          value={email}
-          onChange={handleChange}
-          className="w-full rounded p-2 shadow-inner"
-        />
-
-        <button
-          type="submit"
-          className="flex w-full justify-center rounded bg-slate-800 px-8 py-3 text-white shadow"
+    <div className="flex w-full justify-center bg-shop-bg text-shop-text">
+      <div className="flex w-full max-w-sm flex-col items-center justify-start gap-y-4 px-8 py-16 ">
+        <h3 className="text-2xl font-bold">Subscribe</h3>
+        <span className="text-center text-sm font-light">
+          Sign up with your email address to receive news, updates and deals.
+        </span>
+        <form
+          onSubmit={handleSubmit}
+          className="flex w-full flex-col items-center justify-center gap-y-2"
         >
-          <span className="text-sm font-bold tracking-wider">Sign up</span>
-        </button>
-      </form>
+          <input
+            type="email"
+            placeholder="Email Address"
+            value={email}
+            onChange={handleChange}
+            className="w-full rounded p-2 shadow-inner"
+          />
+
+          <button
+            type="submit"
+            className="flex w-full justify-center rounded bg-slate-800 px-8 py-3 text-white shadow"
+          >
+            <span className="text-sm font-bold tracking-wider">Sign up</span>
+          </button>
+        </form>
+      </div>
     </div>
   );
 };
